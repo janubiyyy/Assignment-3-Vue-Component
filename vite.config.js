03e2d9073976e8c2f4e47e798b4a51a5425774b6
janubiyyy <tasyakhaerani66@gmail.com>
2025-04-13 12:38:48 +0700

    Fix bug and update config; clean up repository by removing node_modules and dist from version control

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      root: "public",
+      plugins: [vue()],
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      build: {
+        lib: {
+          entry: "src/index.js",
+          name: "Greeter",
+          formats: ["umd"],
+          fileName: "compiled-greeter-component",
+        },
+        rollupOptions: {
+          external: ["vue"],
+          output: {
+            globals: {
+              vue: "Vue",
+            },
+          },
+        },
+      },
+    };
+  }
+});
